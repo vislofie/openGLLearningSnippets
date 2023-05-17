084b1f0639d33e79e1dde9ba3969a49ee7d711e8
vislofie <vislofies@gmail.com>
2023-05-17 18:48:53 +0300

    second exercise completed

--- OpenGLFuckAroudn/helloTriangle_exercise_2.cpp
+++ OpenGLFuckAroudn/helloTriangle_exercise_2.cpp
@@ -2,6 +2,20 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const char* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\0";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -26,61 +40,30 @@ int main()
 
 	if (window == NULL)
 	{
-		std::cout << "Failed to create GLFW window" << std::endl;
+		std::cout << "Failed to create window!" << std::endl;
+
 		glfwTerminate();
 		return -1;
 	}
+
 	glfwMakeContextCurrent(window);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
 		return -1;
 	}
 
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	float vertices[] =
-	{
-		-0.5f, -0.5f, 0.0f, // first triangle
-		-0.5f, 0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f, // second triangle
-		0.5f, 0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f
-	};
-
-	unsigned int VBO, VAO;
-	
-
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	const char* vertexShaderSource = "#version 330 core\n"
-			"layout (location = 0) in vec3 aPos;\n"
-			"void main()\n"
-			"{\n"
-			" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-			"}\0";
-
+	// creating shader program
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
-
-	const char* fragmentShaderSource = "#version 330 core\n"
-			"out vec4 FragColor;\n"
-			"void main()\n"
-			"{\n"
-			"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-			"}\0";
 
 	unsigned int fragmentShader;
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -111,9 +94,50 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	// triangles
+
+	float verticesFirstTriangle[]
+	{
+		-0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	float verticesSecondTriangle[]
+	{
+		0.0f, 0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f
+	};
+
+	unsigned int firstVAO, secondVAO;
+	unsigned int firstVBO, secondVBO;
+
+	// first VAO and VBO
+	glGenVertexArrays(1, &firstVAO);
+	glGenBuffers(1, &firstVBO);
+
+	glBindVertexArray(firstVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, firstVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesFirstTriangle), verticesFirstTriangle, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	// second VAO and VBO
+	glGenVertexArrays(1, &secondVAO);
+	glGenBuffers(1, &secondVBO);
+
+	glBindVertexArray(secondVBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, secondVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesSecondTriangle), verticesSecondTriangle, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
@@ -122,18 +146,24 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glBindVertexArray(firstVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glBindVertexArray(secondVBO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(1, &firstVAO);
+	glDeleteVertexArrays(1, &secondVAO);
+	
+	glDeleteBuffers(1, &firstVBO);
+	glDeleteBuffers(1, &secondVBO);
 	glDeleteProgram(shaderProgram);
 
-	glfwTerminate();
-	
 	return 0;
 }
